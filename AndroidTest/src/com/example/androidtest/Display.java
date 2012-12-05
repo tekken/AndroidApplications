15455df0e81a3665299edbaaf4b6bce301934b4b
David Vaz <david.ferd.vaz@gmail.com>
2012-12-05 12:38:11 -0800

    Error: Unable to find Activity

---
+++ AndroidTest/src/com/example/androidtest/Display.java
@@ -0,0 +1,19 @@
+package com.example.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Display extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		TextView name=(TextView)findViewById(R.id.textView1);
+		TextView street=(TextView)findViewById(R.id.textView2);
+		
+		Person person=(Person)getIntent().getExtras().getParcelable("person_details");
+		
+		name.setText(person.getName());
+		street.setText(person.getAddress().getStreet());
+	}
+}
--- AndroidTest/src/com/example/androidtest/Person.java
+++ AndroidTest/src/com/example/androidtest/Person.java
@@ -7,15 +7,19 @@ public class Person implements Parcelable{
 	private String name;
 	private String email;
 	private String contact;
+	private Address address;
 	
 	public Person() {
 		
 	}
 	
 	private Person(Parcel in){
+		
 		name=in.readString();
 		email=in.readString();
 		contact=in.readString();
+		address=new Address();
+		address=in.readParcelable(Address.class.getClassLoader());
 	}
 	
 	public void setName(String strname) {
@@ -30,6 +34,15 @@ public class Person implements Parcelable{
 		this.contact = strcontact;
 	}
 
+
+	public String getName() {
+		return name;
+	}
+
+	public void setAddress(Address address) {
+		this.address=address;
+	}
+	
 	@Override
 	public int describeContents() {
 		return 0;
@@ -40,6 +53,7 @@ public class Person implements Parcelable{
 		dest.writeString(name);
 		dest.writeString(contact);
 		dest.writeString(email);
+		dest.writeParcelable(address, flags);
 	}
 	
 	
@@ -56,8 +70,7 @@ public class Person implements Parcelable{
 		}
 	};
 
-	public String getName() {
-		return name;
+	public Address getAddress() {
+		return address;
 	}
-	
 }
